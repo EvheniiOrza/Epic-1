442e09fabf1c50b1adb4ccfe247b6d5ba730cdd3
EvheniiOrza <yevhenii.orza.shi.2023@lpnu.ua>
2023-10-05 21:39:21 +0300

    vita merge commit

--- main.cpp
+++ main.cpp
@@ -2,19 +2,12 @@
 using namespace std;
 int main(){
     
-<<<<<<< HEAD
-<<<<<<< HEAD
-    cout<< "Evhenii Orza\n"; 
-=======
     cout<< "Evhenii Orza\n";
     cout<<"Veronika Stepaniuk"<<endl;
 
->>>>>>> origin/VeronicaBranchEpic1
-=======
     cout<< "Evhenii Orza\n";
     cout<<"Vita Mostova"<<endl;
 
 
->>>>>>> origin/VitaBranchEpic1
     return 0;
 }
