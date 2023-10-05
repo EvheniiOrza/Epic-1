5afa6faf81c1c8c4f3c1d98146626cc088137991
EvheniiOrza <yevhenii.orza.shi.2023@lpnu.ua>
2023-10-05 21:52:54 +0300

    main commit

--- main.cpp
+++ main.cpp
@@ -3,11 +3,5 @@ using namespace std;
 int main(){
     
     cout<< "Evhenii Orza\n";
-    cout<<"Veronika Stepaniuk"<<endl;
-
-    cout<< "Evhenii Orza\n";
-    cout<<"Vita Mostova"<<endl;
-
-
     return 0;
 }
