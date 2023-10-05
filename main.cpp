90ec90030e1a78707d8e83d8786050a6ac5cf6b3
Stepaniuk <veronika.stepaniuk.shi.2023@lpnu.ua>
2023-10-05 14:39:29 +0300

    Veronica commit

--- main.cpp
+++ main.cpp
@@ -3,7 +3,13 @@ using namespace std;
 int main(){
     
     cout<< "Evhenii Orza\n";
+<<<<<<< HEAD
     
+=======
+    cout<<"Kuzo Ivan"<<endl;
+    cout<<"Veronika Stepaniuk"<<endl;
+
+>>>>>>> 905481b (Fname commit)
 
     return 0;
 }
