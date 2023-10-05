c7dae618ac240870b8d4cda371d501bc36f45093
EvheniiOrza <146078178+EvheniiOrza@users.noreply.github.com>
2023-10-05 21:27:14 +0300

    Update and rename veronicamain.cpp to main.cpp

--- main.cpp
+++ main.cpp
@@ -3,13 +3,7 @@ using namespace std;
 int main(){
     
     cout<< "Evhenii Orza\n";
-<<<<<<< HEAD
-    
-=======
-    cout<<"Kuzo Ivan"<<endl;
     cout<<"Veronika Stepaniuk"<<endl;
-
->>>>>>> 905481b (Fname commit)
 
     return 0;
 }
