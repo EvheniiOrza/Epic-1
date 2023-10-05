ac97c45d9fe60d41a145af878a0bfbeef02b275a
EvheniiOrza <yevhenii.orza.shi.2023@lpnu.ua>
2023-10-05 22:21:27 +0300

    ivan merge

--- main.cpp
+++ main.cpp
@@ -3,18 +3,15 @@ using namespace std;
 int main(){
     
     cout<< "Evhenii Orza\n";
-<<<<<<< HEAD
-<<<<<<< HEAD
+
     cout<< "Vita Mostova"<< endl;
     cout<<"Veronika Stepaniuk\n";
-=======
     
     cout << "Mykyta Shabanov\n";
->>>>>>> origin/MykytaBranchEpic1
-=======
+
     cout<<"Kuzo Ivan\n";
 
->>>>>>> origin/IvanBranchEpic1
+
 
     return 0;
 }
