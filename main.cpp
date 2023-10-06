939a434797be132913cdcbdfa4d6af89af94f8c9
EvheniiOrza <yevhenii.orza.shi.2023@lpnu.ua>
2023-10-06 15:10:22 +0300

    merge commit

--- main.cpp
+++ main.cpp
@@ -3,14 +3,6 @@ using namespace std;
 int main(){
     
     cout<< "Evhenii Orza\n";
-    cout<< "Melnykov Dmytro"<<endl;
-
-    cout<< "Vita Mostova"<< endl;
-    cout<<"Veronika Stepaniuk\n";
-    
-    cout << "Mykyta Shabanov\n";
-
-    cout<<"Kuzo Ivan\n";
 
 
 
