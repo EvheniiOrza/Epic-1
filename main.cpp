1fad5648b5b1a2a83ff40c70ebc1956fdccec866
EvheniiOrza <yevhenii.orza.shi.2023@lpnu.ua>
2023-10-27 10:02:35 +0300

    Marge commit

--- main.cpp
+++ main.cpp
@@ -4,7 +4,17 @@ int main(){
     
     cout<< "Evhenii Orza\n";
 
-    
+    cout<< "Vita Mostova\n";
+
+
+    cout<<"Veronika Stepaniuk"<<endl;
+
+    cout << "Mykyta Shabanov"<<endl;
+
+    cout<<"Kuzo Ivan"<<endl;
+
+    cout<< "Melnykov Dmytro\n";
+
 
     return 0;
 }
